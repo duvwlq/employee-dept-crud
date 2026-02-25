b720a79b866c37378ada15b2745f47558c346e21
duvwlq <dnjs820@naver.com>
2026-02-25 15:30:54 +0900

    Initial commit

---
+++ src/main/java/com/example/employeedeptcrud/EmployeeDeptCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedeptcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDeptCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDeptCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeedeptcrud/EmployeeDeptCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeedeptcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDeptCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
